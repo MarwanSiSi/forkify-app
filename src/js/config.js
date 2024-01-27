d99fa863e2f4eb0c36c410b68d3a09477073904d
MarwanSiSi <marwan.elsisi21@gmail.com>
2024-01-28 03:35:13 +0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'b9cd185e-fe90-4fab-8570-2ec07dcec86e';
+export const MODAL_CLOSE_SEC = 2.5;
